396784111748e54959a8cf2bbf92a97235ab9a58
BIW <biwtfz.tnt@gmail.com>
2019-09-05 15:13:15 +0700

    first commit

---
+++ app/Http/Controllers/Admin/UserApiController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\User;
+
+class UserApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::with(['roles'])->get();
+        return $users;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $form = $request->all();
+        $user = new User();
+        $user->fill($form);
+        $user->password =bcrypt($form['password']);
+        $user->save();
+
+        $roles = $form['roles'];
+        $ids = array_pluck($roles, 'id');
+        $user->roles()->sync($ids);
+
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $form = $request->get();
+        $user = User::find($id);
+        $user->fill($form);
+        $user->save();
+
+        $rid = $form['role']['id'];
+        $admin = Role::find($rid);
+        $user->attachRole($admin);
+        $user->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $result = User::destroy($id);
+        return [$result];
+    }
+}
